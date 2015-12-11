4990f6c444d7497de66d3c79b4904b64c9c02738
Carson Jones <carsoncjones@gmail.com>
2015-12-10 18:41:18 -0600

    changing code to hello world, let's see this baby run before doing anythin fancy

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.redirect('https://wiki.doctorlogic.com/dosearchsite.action?queryString=' + req.query.q);
+  res.send('<h1>Hello World!</h1>');
+  // res.redirect('https://wiki.doctorlogic.com/dosearchsite.action?queryString=' + req.query.q);
 });
 
 var server = app.listen(8888, function() {
